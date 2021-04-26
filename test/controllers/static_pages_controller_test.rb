1b9ab37c7f67d444f18743f4d32123fe0614e717
okdhrk <okdhrk1014@gmail.com>
2021-04-26 09:58:12 +0000

    controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # 「Homeページのテスト。GETリクエストをhomeアクションに対して発行 (=送信) せよ。そうすれば、リクエストに対するレスポンスは[成功]になるはず」
   test "should get home" do
     get static_pages_home_url
     assert_response :success
