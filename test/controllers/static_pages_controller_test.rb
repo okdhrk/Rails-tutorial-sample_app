ce84a2b3545963e9081d8672df9e0ad2b954ef4c
okdhrk <okdhrk1014@gmail.com>
2021-04-26 11:29:20 +0000

    リファクタリング static-pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
-  
+
   # 「Homeページのテスト。GETリクエストをhomeアクションに対して発行 (=送信) せよ。そうすれば、リクエストに対するレスポンスは[成功]になるはず」
   test "should get home" do
     get static_pages_home_url
@@ -19,7 +19,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
-  
+
   test "should get about" do
     get static_pages_about_url
     assert_response :success
