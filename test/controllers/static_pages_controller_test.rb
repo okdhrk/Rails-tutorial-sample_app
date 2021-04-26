a907d964710f4f5e52e4bdadddcc539435f68785
okdhrk <okdhrk1014@gmail.com>
2021-04-26 13:40:23 +0000

    演習3.4.4 rootをhomeに設定

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,12 +6,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  # 「Homeページのテスト。GETリクエストをhomeアクションに対して発行 (=送信) せよ。そうすれば、リクエストに対するレスポンスは[成功]になるはず」
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
-    # assert_selectメソッドでは、特定のHTMLタグが存在するかどうかをテストします
-    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
