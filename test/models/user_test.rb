75be02850941357112c2be10aad611f31b26b0c0
okdhrk <okdhrk1014@gmail.com>
2021-05-09 12:51:24 +0000

    ユーザーがセキュアなパスワードを持っている

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   # テストを実行する前に動く
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid?" do
@@ -43,13 +44,24 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-  
+
   # 重複するメールアドレス拒否テスト
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  # パスワードの最小化
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
