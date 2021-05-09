560ce94dd8d7a0c30ac7c207db8d10f1b6d4a876
okdhrk <okdhrk1014@gmail.com>
2021-05-09 09:06:53 +0000

    一意性を検証する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
 
+  # email属性を小文字に変換してメールアドレスの一意性を保証する
+  before_save { email.downcase! }
+
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -43,5 +43,13 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+  
+  # 重複するメールアドレス拒否テスト
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 
 end
