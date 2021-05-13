d4cc798221d771b6c210710d22eb7e8f46c14871
okdhrk <okdhrk1014@gmail.com>
2021-05-13 09:06:25 +0000

    正しいフォーム

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
 end
