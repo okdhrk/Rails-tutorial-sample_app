2fff73e62ee2cd0a9ec19e0f9cb68335169c9ed7
okdhrk <okdhrk1014@gmail.com>
2021-05-17 01:25:40 +0000

    ユーザーの検索と認証

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページをリダイレクトする
+    else
+      # エラーメッセージを作成
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
